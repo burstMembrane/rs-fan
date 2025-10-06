2e57964311478a4786ff46e942ffa0ac56a302c5
Liam Power <liamfpower@gmail.com>
2025-10-07 10:17:22 +1100

    chore: version bump

--- python/resamplefan/__init__.py
+++ python/resamplefan/__init__.py
@@ -2,10 +2,10 @@
 resamplefan - Fast audio resampling using SoXR
 
 This module provides a Python interface to the resamplefan audio resampling library,
-which uses the high-quality SoXR (SoX Resampler) library.
+which uses the high-quality SoXR (SoX Resampler) library and lame for MP3 support.
 """
 
 from .resamplefan import resample_fan, resample_fan_batch, set_num_threads
 
 __all__ = ["resample_fan", "resample_fan_batch", "set_num_threads"]
-__version__ = "0.1.0"
+__version__ = "0.2.0"
